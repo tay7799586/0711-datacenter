c2c0970fd56bf0634d4fa4bd922082fcb70d8648
tay7799586 <tay7799586@gmail.com>
2022-07-07 16:34:27 +0800

    https://github.com/tay7799586/0707-datacenter.git

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from mysite.models import News
 import random
 
 def index(request):
-    name= "Meine Dammen und Herren"
-    
+    name= "News Information"
+    news=News.objects.all()
     return render(request, "index.html", locals())
 
 def lotto(request):
@@ -11,3 +12,7 @@ def lotto(request):
     random.shuffle(lotto)
     lotto = lotto[:6]
     return render(request, "lotto.html", locals())
+
+def show(request, id):
+    item= News.objects.get(id=id)
+    return render(request, "show.html" , locals())
