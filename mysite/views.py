269778f40e1be7dcf867c2b382473b5ffb2d9b29
tay7799586 <tay7799586@gmail.com>
2022-07-07 13:18:45 +0800

    first

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+def index(request):
+    name= "Meine Dammen und Herren"
+    
+    return render(request, "index.html", locals())
+
+def lotto(request):
+    lotto = [i for i in range(1,50)]
+    random.shuffle(lotto)
+    lotto = lotto[:6]
+    return render(request, "lotto.html", locals())
